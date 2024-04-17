cead4c3d79b02eea63e194a353e2fea94401e0c6
jossjic <jossjic_03@hotmail.com>
2024-04-16 18:54:18 -0600

    middleWare for dev

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import crypto from "crypto";
 
 const app = express();
 
+// Middleware para permitir solicitudes desde localhost:5173
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Middleware para analizar el cuerpo de las solicitudes como JSON
 app.use(bodyParser.json());
 
